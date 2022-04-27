66e47e90b6eed4afaa55f43378c611039486e29c
Joel Ramirez <joelramirez@Joels-MBP.home>
2022-04-27 12:32:26 -0400

    added changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "01dc3a215d9ebe70"; }
+/******/ 		__webpack_require__.h = function() { return "2ad7294575e332b2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
